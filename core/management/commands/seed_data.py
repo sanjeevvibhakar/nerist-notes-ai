b1a69f01472a53e81d459ece73f69e9479786958
Sanjeev Vibhakar <sanjeevvibhakar@outlook.com>
2026-01-15 20:52:57 +0530

    Fix: Remove non-existent code field from seed_data command

--- core/management/commands/seed_data.py
+++ core/management/commands/seed_data.py
@@ -8,9 +8,9 @@ class Command(BaseCommand):
         self.stdout.write('Seeding data...')
 
         # 1. Create Departments
-        cse, _ = Department.objects.get_or_create(name='Computer Science & Engineering', code='CSE')
-        ece, _ = Department.objects.get_or_create(name='Electronics & Communication', code='ECE')
-        me, _ = Department.objects.get_or_create(name='Mechanical Engineering', code='ME')
+        cse, _ = Department.objects.get_or_create(name='Computer Science & Engineering')
+        ece, _ = Department.objects.get_or_create(name='Electronics & Communication')
+        me, _ = Department.objects.get_or_create(name='Mechanical Engineering')
 
         # 2. Create Years for CSE (as example)
         for i in range(1, 5):
@@ -21,13 +21,13 @@ class Command(BaseCommand):
 
             # Add sample subjects for Semesters
             if i == 1:
-                sub1, _ = Subject.objects.get_or_create(name='Physics', code='PH101')
-                sub2, _ = Subject.objects.get_or_create(name='Mathematics-I', code='MA101')
+                sub1, _ = Subject.objects.get_or_create(name='Physics')
+                sub2, _ = Subject.objects.get_or_create(name='Mathematics-I')
                 SubjectOffering.objects.get_or_create(subject=sub1, semester=sem1)
                 SubjectOffering.objects.get_or_create(subject=sub2, semester=sem1)
             
             if i == 3:
-                sub, _ = Subject.objects.get_or_create(name='Operating Systems', code='CS301')
+                sub, _ = Subject.objects.get_or_create(name='Operating Systems')
                 SubjectOffering.objects.get_or_create(subject=sub, semester=sem1)
 
         self.stdout.write(self.style.SUCCESS('Successfully seeded data!'))
